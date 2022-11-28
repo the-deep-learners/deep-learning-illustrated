69fc4dd5800216182f20919ffbae25b9aac97856
Piotr Mikosz <mikosz.piotr@gmail.com>
2022-11-28 20:05:30 +0000

    Add image building before running the containter

--- rundocker.bat
+++ rundocker.bat
@@ -1,1 +1,2 @@
+docker build -t dli-stack .
 docker run -v %cd%:/home/jovyan/work -it --rm -p 8888:8888 -p 6006:6006 dli-stack
--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo docker build -t dli-stack .
 sudo docker run -v $(pwd):/home/jovyan/work -it --rm -p 8888:8888 -p 6006:6006 dli-stack
